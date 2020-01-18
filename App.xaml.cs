08733805862081e6bdf3468d93863f66bfa9c985
Wojciech Marciniak <nnown1@gmail.com>
2020-01-18 17:46:34 +0100

    Added Pagination

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,12 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ProjektPP2
-{
+namespace ProjektPP2 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
+        public static MainWindow ParentWindowRef;
     }
 }
---
+++ pages/addPage.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+    namespace ProjektPP2 {
+    public partial class addPage : Page {
+        public addPage() {
+            InitializeComponent();
+        }
+    }
+}
